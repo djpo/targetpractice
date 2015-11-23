5a8a42d57d2c8af3149424a5eb3a8087ab06a27e
djpo <derekpoppe@gmail.com>
2015-11-23 11:36:26 -0800

    vizSpots light up when discs are on them

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
-var pigeon = 0;
+////////// global variables //////////
+var range = [];
+var discs = [];
+var numOfDiscs = 3;
 
+////////// functions //////////
+function createSpotObjects() {
+	for (i = 0; i < 10; i++) {
+		range[i] = {'hasDiscs': false};
+	}
+};
+function createDiscs () {
+	for (i = 0; i < numOfDiscs; i++) {
+		discs.push({});
+		discs[i].position = Math.floor(Math.random() * 10);
+		discs[i].velocity = Math.ceil(Math.random() * 5);
+	}
+}
+function setRangeHasDiscs () {
+	for (i = 0; i < discs.length; i++) {
+		range[discs[i].position].hasDiscs = true;
+	}
+}
+function changeVizSpotClasses () {
+	for (i = 0; i < range.length; i++) {
+		if (range[i].hasDiscs) {
+			$('#s' + i).addClass('hasDiscs');
+		} else {
+			$('#s' + i).removeClass('hasDiscs');
+		}
+	}
+}
+
+////////// run commands, test //////////
+createSpotObjects();
+createDiscs();
+
+$(discs).each(function (i) {
+	console.log('discs[' + i + '].position: ' + discs[i].position);
+});
+
+setRangeHasDiscs();
+changeVizSpotClasses();
+
+console.log('__________');
+
+////////// in progress //////////
