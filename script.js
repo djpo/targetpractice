d7a2c7a0bc1b187b7c1b48511c9d16763171625a
djpo <derekpoppe@gmail.com>
2015-11-23 12:56:08 -0800

    discs bounce off ends of range

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 ////////// global variables //////////
 var range = [];
 var discs = [];
+var sizeOfRange = 10;
 var numOfDiscs = 3;
 
 ////////// functions //////////
 function createSpotObjects() {
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < sizeOfRange; i++) {
 		range[i] = {'hasDiscs': false};
 	}
 };
 function createDiscs () {
 	for (i = 0; i < numOfDiscs; i++) {
 		discs.push({});
-		discs[i].position = Math.floor(Math.random() * 10);
-		discs[i].velocity = Math.ceil(Math.random() * 5);
+		discs[i].position = Math.floor(Math.random() * range.length);
+		// discs[i].velocity = Math.ceil(Math.random() * 3);
+		// below just to test
+		discs[i].velocity = 1;
 	}
 }
 function setRangeHasDiscs () {
+	for (i = 0; i < range.length; i++) {
+		range[i].hasDiscs = false;
+	}
 	for (i = 0; i < discs.length; i++) {
 		range[discs[i].position].hasDiscs = true;
 	}
@@ -30,18 +36,44 @@ function changeVizSpotClasses () {
 		}
 	}
 }
+function updatePositions() {
+	$(discs).each(function (i) {
+		var last = range.length - 1;
+		this.position += this.velocity;
+		if (this.position < 0) {
+			this.position = -this.position;
+			this.velocity = -this.velocity;
+		} else if (this.position > last) {
+			var diff = this.position - (last);
+			this.position = last - diff;
+			this.velocity = -this.velocity;
+		}
+	});
+}
+function updateViz () {
+	setRangeHasDiscs();
+	changeVizSpotClasses();
+}
+
+function update () {
+	updatePositions();
+	updateViz();
+};
+function startGame () {
+	update();
+	gameInterval = setInterval(update, 800);
+}
+function stopGame () {
+	clearInterval(gameInterval);
+}
 
 ////////// run commands, test //////////
-createSpotObjects();
-createDiscs();
-
-$(discs).each(function (i) {
-	console.log('discs[' + i + '].position: ' + discs[i].position);
+$(document).ready(function () {
+	createSpotObjects();
+	createDiscs();
 });
 
-setRangeHasDiscs();
-changeVizSpotClasses();
-
-console.log('__________');
+// console.log('__________');
 
 ////////// in progress //////////
+
